db52e56040705b09d14e169fbd11975cd3de6ee0
claudim <claudimarco@hotmail.it>
2020-06-12 13:06:00 +0200

    improved code

--- src/main/java/com/univaq/stoneage/model/MResource.java
+++ src/main/java/com/univaq/stoneage/model/MResource.java
@@ -6,6 +6,10 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 import java.io.Serializable;
 
+/**
+ * Is a persistent entity.
+ * It knows its type.
+ */
 @Entity
 @Table(name = "resource")
 public class MResource implements Serializable {
@@ -13,21 +17,36 @@ public class MResource implements Serializable {
     @Column(name = "resource_type", nullable = true, unique = true, length = 30)
     private String m_type;
 
+    /**
+     * Constructor.
+     */
     public MResource() {
-
     }
 
+    /**
+     * Constructor.
+     *
+     * @param m_type The resource type
+     */
     public MResource(String m_type) {
         this.m_type = m_type;
     }
 
+    /**
+     * Get the resource type.
+     *
+     * @return The resource type
+     */
     public String getM_type() {
         return m_type;
     }
 
+    /**
+     * Set the resource type.
+     *
+     * @param m_type The resource type
+     */
     public void setM_type(String m_type) {
         this.m_type = m_type;
     }
-
-
 }
