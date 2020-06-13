d87058ff245832ea1b00973f0c512309cfd98fa3
ricman0 <mantini.riccardo@gmail.com>
2020-06-13 15:53:39 +0200

    Commented code in nextid package

--- src/main/java/com/univaq/stoneage/model/nextId/GetFirstIdStrategy.java
+++ src/main/java/com/univaq/stoneage/model/nextId/GetFirstIdStrategy.java
@@ -2,12 +2,29 @@ package com.univaq.stoneage.model.nextId;
 
 import java.util.List;
 
+/**
+ * Strategy that returns first index
+ */
 public class GetFirstIdStrategy implements IGetNextIdStrategy {
+
+    /**
+     * Returns the first index in range
+     *
+     * @param min inferior bound
+     * @param max superior bound
+     * @return first index in range
+     */
     @Override
     public int getNextId(int min, int max) {
         return min;
     }
 
+    /**
+     * Returns first index in list
+     *
+     * @param list list in which search the next id
+     * @return first index in given list
+     */
     @Override
     public int getNextId(List<Object> list) {
         return 0;
